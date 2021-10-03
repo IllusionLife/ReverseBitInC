a11892fdb419d363ba0859172c78328db4e219c7
IllusionLife <30587357+IllusionLife@users.noreply.github.com>
2021-10-03 13:09:25 +0300

    Update and rename ReverseByte to ReverseByte.c

--- ReverseByte.c
+++ ReverseByte.c
@@ -5,12 +5,12 @@ int main() {
     printf("Enter a char to be reversed:\n");
     scanf("%c", &ucInputByte);
 
-    int iCharBitValue = 0, iCharDecValue = (int)ucInputByte;
+    int iCharBitValue = 0;
 
     for(int i = 8; i != 0; i--) {
         iCharBitValue *= 10;
-        iCharBitValue = iCharBitValue + (iCharDecValue % 2);
-        iCharDecValue /= 2;
+        iCharBitValue = iCharBitValue + (ucInputByte % 2);
+        ucInputByte /= 2;
     }
 
     printf("The reversed char's bits look like this: %08d", iCharBitValue);
